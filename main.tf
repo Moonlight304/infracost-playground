15ce62ffb020bfacdf9cead88309290373e43fe5
Nitin Jagadish <123298743+Moonlight304@users.noreply.github.com>
2026-05-28 18:39:58 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "this" {
+data "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
 }
@@ -6,28 +6,28 @@ resource "azurerm_resource_group" "this" {
 resource "azurerm_virtual_network" "this" {
   name                = "${var.vm_name}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
 }
 
 resource "azurerm_subnet" "this" {
   name                 = "${var.vm_name}-subnet"
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "this" {
   name                = "${var.vm_name}-pip"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
   allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "this" {
   name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
 
   ip_configuration {
     name                          = "internal"
@@ -53,8 +53,8 @@ resource "azurerm_linux_virtual_machine" "this" {
   count = var.os_type == "linux" ? 1 : 0
 
   name                = var.vm_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
   size                = var.vm_size
   admin_username      = var.admin_username
 
@@ -82,8 +82,8 @@ resource "azurerm_windows_virtual_machine" "this" {
   count = var.os_type == "windows" ? 1 : 0
 
   name                = var.vm_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
   size                = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
